20b9b7134fcdf8b9539938f54cc9587177b1bf3e
mv235 <izmailoff.ehduard@yandex.ru>
2024-11-16 21:37:20 +0300

    Домашняя работа Переменные№1

--- src/Main.java
+++ src/Main.java
@@ -7,72 +7,64 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
-
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        System.out.println("dog=" +dog + " cat=" + cat + " paper=" + paper);
 
         System.out.println("Задание№2");
 
-        var dog1 = 8.0 + 4;
-        var cat1 = 3.6 + 4;
-        var paper1 = 763789 + 4;
+        var dogAdd = (dog+4);
+        System.out.println(dogAdd);
+        var catAdd = (cat+4);
+        System.out.println(catAdd);
+        var paperAdd = (paper+4);
+        System.out.println(paperAdd);
+        System.out.println("dog+4=" + dogAdd + " cat+4=" + catAdd + " paper+4=" + paperAdd);
 
-        System.out.println(dog1);
-        System.out.println(cat1);
-        System.out.println(paper1);
 
         System.out.println("Задание№3");
 
-        var dog2 = 8.0 + 4 - 3.5;
-        var cat2 = 3.6 + 4 - 1.6;
-        var paper2 = 763789 + 4 - 7639;
+        var dogShrink = (dogAdd-3.5);
+        System.out.println(dogShrink);
+        var catShrink = (catAdd-1.6);
+        System.out.println(catShrink);
+        var paperShrink = (paperAdd-7639);
+        System.out.println(paperShrink);
+        System.out.println("dogAdd-3.5=" + dogShrink + " catAdd-1.6=" + catShrink + " paperAdd-7639=" + paperShrink);
 
-        System.out.println(dog2);
-        System.out.println(cat2);
-        System.out.println(paper2);
 
         System.out.println("Задание№4");
 
         var friend = 19;
-        var friend1 = 19 + 2;
-        var friend2 = friend1 / 7;
-
+        friend += 2;
+        friend /= 7;
         System.out.println(friend);
-        System.out.println(friend1);
-        System.out.println(friend2);
+
 
         System.out.println("Задание№5");
 
         var frog = 3.5;
-        var frog1 = 3.5 * 10;
-        var frog2 = frog1 / 3.5;
-        var frog3 = frog2 + 4;
-
+        frog *=10;
+        frog /=3.5;
+        frog +=4;
         System.out.println(frog);
-        System.out.println(frog1);
-        System.out.println(frog2);
-        System.out.println(frog3);
 
         System.out.println("Задание№6");
 
         var Boxer1 = 78.2;
         var Boxer2 = 82.7;
-        var TotalWeight = Boxer1 + Boxer2;
+        var totalWeight = Boxer1 + Boxer2;
         var Difference = Boxer2 - Boxer1;
 
         System.out.println(Boxer1);
         System.out.println(Boxer2);
-        System.out.println(TotalWeight);
+        System.out.println(totalWeight);
         System.out.println(Difference);
+        System.out.println("Масса первого боксера " + Boxer1 + " Масса второго боксера " + Boxer2 + " общая масса бойцов " + totalWeight + " разница в вессе " + Difference) ;
 
         System.out.println("Задание№7");
 
-        var DifferenceWeight1 = Boxer2 - Boxer1;
-        var DifferenceWeight2 = Boxer2 % Boxer1;
+        var remainderOfDivision = Boxer2 % Boxer1;
+        System.out.println(remainderOfDivision);
 
-        System.out.println(DifferenceWeight1);
-        System.out.println(DifferenceWeight2);
 
         System.out.println("Задание№8.1");
 
@@ -88,7 +80,8 @@ public class Main {
         System.out.println("Задание 8.2");
 
         var NumberOfEmployees1 = NumberOfEmployees + 94;
-        var TotalTime1 = WorkingTime * NumberOfEmployees1;
+        var TotalTime1 = TotalTime / NumberOfEmployees1;
+
 
         System.out.println(NumberOfEmployees1);
         System.out.println(TotalTime1);
